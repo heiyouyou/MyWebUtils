72ed788b0cf80cee5ad4fe781e1b1c2f6cf45d98
Wells <heiyouyou@users.noreply.github.com>
2018-08-03 10:24:04 +0800

    add
    add others js utils functions

--- js/wzy_utils.js
+++ js/wzy_utils.js
@@ -2360,3 +2360,92 @@ const merge = (target, ...sources) => Object.assign(target, ...sources);
 
 // 返回一个新的扩展对象
 const mergeNew = (...sources) => Object.assign({}, ...sources);
+
+//保留两位小数 
+//功能：将浮点数四舍五入，取小数点后2位 
+function toDecimal(x) { 
+	var f = parseFloat(x); 
+	if (isNaN(f)) { 
+		return; 
+	} 
+	f = Math.round(x*100)/100; 
+	return f; 
+} 
+
+//制保留2位小数，如：2，会在2后面补上00.即2.00 
+function toDecimal2(x) { 
+	var f = parseFloat(x); 
+	if (isNaN(f)) { 
+		return false; 
+	} 
+	var f = Math.round(x*100)/100; 
+	var s = f.toString(); 
+	var rs = s.indexOf('.'); 
+	if (rs < 0) { 
+		rs = s.length; 
+		s += '.'; 
+	} 
+	while (s.length <= rs + 2) { 
+		s += '0'; 
+	} 
+	return s; 
+} 
+   
+// src：需要格式化的数字，pos：指数
+function fomatFloat(src,pos){  
+	return Math.round(src*Math.pow(10, pos))/Math.pow(10, pos);  
+}
+
+// codePointAt方法是测试一个字符由两个字节还是由四个字节组成
+function is32Bit(c) {
+  	return c.codePointAt(0) > 0xFFFF;
+}
+
+var HtmlUtil = {
+  /*1.用浏览器内部转换器实现html转码*/
+  htmlEncode: function (html) {
+    //1.首先动态创建一个容器标签元素，如DIV
+    var temp = document.createElement("div");
+    //2.然后将要转换的字符串设置为这个元素的innerText(ie支持)或者textContent(火狐，google支持)
+    (temp.textContent != undefined) ? (temp.textContent = html) : (temp.innerText = html);
+    //3.最后返回这个元素的innerHTML，即得到经过HTML编码转换的字符串了
+    var output = temp.innerHTML;
+    temp = null;
+    return output;
+  },
+  /*2.用浏览器内部转换器实现html解码*/
+  htmlDecode: function (text) {
+    //1.首先动态创建一个容器标签元素，如DIV
+    var temp = document.createElement("div");
+    //2.然后将要转换的字符串设置为这个元素的innerHTML(ie，火狐，google都支持)
+    temp.innerHTML = text;
+    //3.最后返回这个元素的innerText(ie支持)或者textContent(火狐，google支持)，即得到经过HTML解码的字符串了。
+    var output = temp.innerText || temp.textContent;
+    temp = null;
+    return output;
+  },
+  /*3.用正则表达式实现html转码*/
+  htmlEncodeByRegExp: function (str) {
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&/g, "&amp;");
+    s = s.replace(/</g, "&lt;");
+    s = s.replace(/>/g, "&gt;");
+    s = s.replace(/ /g, "&nbsp;");
+    s = s.replace(/\'/g, "&#39;");
+    s = s.replace(/\"/g, "&quot;");
+    return s;
+  },
+  /*4.用正则表达式实现html解码*/
+  htmlDecodeByRegExp: function (str) {
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&amp;/g, "&");
+    s = s.replace(/&lt;/g, "<");
+    s = s.replace(/&gt;/g, ">");
+    s = s.replace(/&nbsp;/g, " ");
+    s = s.replace(/&#39;/g, "\'");
+    s = s.replace(/&quot;/g, "\"");
+    return s;
+  }
+}
